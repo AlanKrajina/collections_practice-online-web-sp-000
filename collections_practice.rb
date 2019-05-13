a4a82a7a7136c68fa3ca3c649104915879e78e5c
Alan Krajina <alankrajina88@gmail.com>
2019-05-13 09:52:31 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,53 @@
+def sort_array_asc(int)
+  int.sort
+end
 
+def sort_array_desc(int)
+  int.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  return array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |word|
+      word[2]="$"
+  end
+  array
+end
+
+def find_a(array)
+  new_array=[]
+  array.each do |word|
+    if word.start_with?("a")
+      new_array << word
+    end
+  end
+  return new_array
+end
+
+def sum_array(array)
+  array.inject(:+)
+end
+
+def add_s(array)
+  new_array=[]
+  array.each_with_index.collect do |element, index| 
+
+    new_array= element << "s" 
+  end
+end
+
+add_s(["hand","feet", "knee", "table"])
