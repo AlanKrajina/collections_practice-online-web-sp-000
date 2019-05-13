eefacae1690a5ea5035bcb2df0f189a5bafef0b2
Alan Krajina <alankrajina88@gmail.com>
2019-05-13 09:57:29 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -45,7 +45,7 @@ end
 def add_s(array)
   new_array=[]
   array.each_with_index.collect do |element, index| 
-   if !element.end_with?("s")
+   if element.end_with?("s")
     new_array= element << "s" 
   end
 end
